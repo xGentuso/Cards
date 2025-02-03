f148f75585ece2aa64c496f3f9455508e332ce2a
xGentuso <ryan.mota@triosstudent.com>
2025-02-03 10:09:48 -0500

    end of chapter 13 plus challenges

--- Cards/Views/CardsListView.swift
+++ Cards/Views/CardsListView.swift
@@ -1,33 +1,31 @@
-//
-//  CardsListView.swift
-//  Cards
-//
-//  Created by ryan mota on 2025-02-03.
-//
-
 import SwiftUI
 
 struct CardsListView: View {
-    
-    @State private var isPresented = false
+  @State private var isPresented = false
 
-    var body: some View {
-        ScrollView(showsIndicators: false) {
-            VStack {
-                ForEach(0..<10) { _ in
-                    CardThumbnail()
-                        .onTapGesture {
-                          isPresented = true
-                        }
-                }
+  var body: some View {
+    list
+    .fullScreenCover(isPresented: $isPresented) {
+      SingleCardView()
+    }
+  }
+
+  var list: some View {
+    ScrollView(showsIndicators: false) {
+      VStack {
+        ForEach(0..<10) { _ in
+          CardThumbnail()
+            .onTapGesture {
+              isPresented = true
             }
         }
-        .fullScreenCover(isPresented: $isPresented) {
-          SingleCardView()
-        }
+      }
     }
+  }
 }
 
-#Preview {
+struct CardsListView_Previews: PreviewProvider {
+  static var previews: some View {
     CardsListView()
+  }
 }
