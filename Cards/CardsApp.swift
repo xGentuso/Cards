46af11db791061c91bfb58a9ec53336048095b52
xGentuso <ryan.mota@triosstudent.com>
2025-02-03 08:15:40 -0500

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by ryan mota on 2025-02-03.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
